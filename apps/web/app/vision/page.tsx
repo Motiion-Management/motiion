72319730cc69fb5ff18fbc9bbebf5fbd7d3f035f
Andy Terwilliger <andyterwilliger1@gmail.com>
2024-04-29 21:33:18 -0500

    Carousel component added, button text color changed

--- apps/web/app/vision/page.tsx
+++ apps/web/app/vision/page.tsx
@@ -1,28 +1,68 @@
+"use client"
 import Link from 'next/link'
+import { useState, useEffect } from 'react'
 import { Button, buttonVariants } from '@/components/ui/button'
-import { Pagination, PaginationContent, PaginationItem, PaginationLink } from '@/components/ui/pagination'
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle
-} from '@/components/ui/card'
-import { Checkbox } from '@/components/ui/checkbox'
-import { Input } from '@/components/ui/input'
+  Carousel,
+  CarouselItem,
+  CarouselContent,
+  CarouselApi,
+  CarouselNext,
+  CarouselPrevious
+} from '@/components/ui/carousel'
 
 export default function Vision() {
-    return (
-    <div className="flex min-h-screen w-full flex-col">
-      <main className="bg-background flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10 justify-between">
-        <div className="mx-auto grid w-full max-w-6xl gap-2 my-24">
-          <h1 className="text-4xl font-semibold pb-10">The dance ecosystem in <span className="text-accent">motiion</span></h1>
-          <p className="text-xl">Our mission is to empower dancers with innovative tools and resources to enhance their mental health, financial stability, and creative expression, fostering a vibrant and sustainable ecosystem.</p>
-        </div>
-        <div className="mx-auto grid w-full max-w-6xl gap-2 mb-8">
-        <Button className="rounded-full font-semibold" size="rounded" variant="default">Next</Button>
+  const [api, setApi] = useState<CarouselApi>()
 
+  const handleNextClick = () => {
+    if (api) {
+      api.scrollNext()
+
+    }
+  }
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <main className="bg-background flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col justify-between gap-4 p-4 md:gap-8 md:p-10">
+        <Carousel  setApi={setApi}>
+          <CarouselContent>
+              <>
+            <CarouselItem>
+              <div className="mx-auto my-24 grid w-full max-w-6xl gap-2">
+                <h1 className="pb-10 text-4xl font-semibold">
+                  The dance ecosystem in{' '}
+                  <span className="text-accent">motiion</span>
+                </h1>
+                <p className="text-xl">
+                  Our mission is to empower dancers with innovative tools and
+                  resources to enhance their mental health, financial stability,
+                  and creative expression, fostering a vibrant and sustainable
+                  ecosystem.
+                </p>
+              </div>
+            </CarouselItem>
+            <CarouselItem>
+              <div className="mx-auto my-24 grid w-full max-w-6xl gap-2">
+                <h2 className="pb-10 text-4xl font-semibold">
+                  Update resume.
+                  <br />
+                  <span className="text-accent">Manage headshots.</span>
+                  <br /> & more...
+                </h2>
+              </div>
+            </CarouselItem>
+            </>
+          </CarouselContent>
+        </Carousel>
+
+        <div className="mx-auto mb-8 grid w-full max-w-6xl gap-2">
+          <Button onClick={handleNextClick}
+            className="rounded-full font-semibold text-background"
+            size="rounded"
+            variant="default"
+           
+          >
+            Next
+          </Button>
         </div>
       </main>
     </div>
