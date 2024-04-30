3bdf37bf1cc182646e66546e90e90e51ce59c853
Jake Hebert <jacobshebert@gmail.com>
2024-04-30 09:28:37 -0500

    Feature/mo 19 verify auth setup (#1)
    * Set up custom sign in and sign up pages
    
    * update styling of clerk forms

---
+++ apps/web/app/(auth)/layout.tsx
@@ -0,0 +1,3 @@
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <div className="grid min-h-screen place-items-center">{children}</div>
+}
--- apps/web/app/ConvexClientProvider.tsx
+++ apps/web/app/ConvexClientProvider.tsx
@@ -1,28 +1,30 @@
-'use client';
+'use client'
+import { ReactNode } from 'react'
+import { ConvexReactClient } from 'convex/react'
+import { ConvexProviderWithClerk } from 'convex/react-clerk'
+import { ClerkProvider, useAuth } from '@clerk/clerk-react'
 
-import { ReactNode } from 'react';
-import { ConvexReactClient } from 'convex/react';
-import { ConvexProviderWithClerk } from 'convex/react-clerk';
-import { ClerkProvider, useAuth } from '@clerk/clerk-react';
-import { ErrorBoundary } from './ErrorBoundary';
-
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!)
 
 export default function ConvexClientProvider({
-  children,
+  children
 }: {
-  children: ReactNode;
+  children: ReactNode
 }) {
   return (
-    // NOTE: Once you get Clerk working you can remove this error boundary
-    <ErrorBoundary>
-      <ClerkProvider
-        publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
-      >
-        <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-          {children}
-        </ConvexProviderWithClerk>
-      </ClerkProvider>
-    </ErrorBoundary>
-  );
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+      appearance={{
+        elements: {
+          formButtonPrimary: 'bg-secondary hover:bg-primary/90 ',
+          input:
+            'bg-background !border !border-border text-text focus-within:ring-2 focus-within:ring-ring focus-within:ring-offset-2 focus-within:!shadow-none'
+        }
+      }}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  )
 }
