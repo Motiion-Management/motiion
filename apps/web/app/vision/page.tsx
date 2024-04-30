58d9f628430f6a3806238abb14d5160dcf5d7f0d
Andy Terwilliger <andyterwilliger1@gmail.com>
2024-04-30 13:05:53 -0500

    Remove unused state variables and imports

--- apps/web/app/vision/page.tsx
+++ apps/web/app/vision/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import Link from 'next/link'
 import { useState, useEffect } from 'react'
 import { Button, buttonVariants } from '@/components/ui/button'
 
@@ -8,21 +7,16 @@ import {
   CarouselItem,
   CarouselContent,
   CarouselApi,
-  CarouselNext,
-  CarouselPrevious
 } from '@/components/ui/carousel'
-import { set } from 'date-fns'
 
 export default function Vision() {
   const [api, setApi] = useState<CarouselApi>()
   const [current, setCurrent] = useState(1)
-  const [count, setCount] = useState(0)
   useEffect(() => {
     if (!api) {
       return
     }
 
-    setCount(api.scrollSnapList().length)
     setCurrent(api.selectedScrollSnap() + 1)
 
     api.on('select', () => {
@@ -35,7 +29,7 @@ export default function Vision() {
     }
   }
   return (
-    <div className="flex min-h-screen w-full flex-col">
+    <div className="flex min-h-screen w-full flex-col ">
       <main className="bg-background flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col justify-between gap-4 p-4 md:gap-8 md:p-10">
         <Carousel setApi={setApi}>
           <CarouselContent>
@@ -76,15 +70,14 @@ export default function Vision() {
           </CarouselContent>
         </Carousel>
         <div className="mx-auto mb-8 grid w-full max-w-6xl gap-2">
-          <div className="flex justify-center pb-5">
-            <svg
+          <div className="flex flex-col justify-center items-center pb-3">
+            <svg className="mb-2"
               xmlns="http://www.w3.org/2000/svg"
               width="44"
               height="8"
               viewBox="0 0 44 8"
               fill="none"
             >
-              <script xmlns="" />
               <circle
                 cx="4"
                 cy="4"
@@ -103,12 +96,12 @@ export default function Vision() {
                 r="4"
                 fill={api?.selectedScrollSnap() === 2 ? '#00CCB6' : '#DFE3E7'}
               />
-              <script xmlns="" />
             </svg>
           </div>
+            <hr className="w-screen" />
           <Button
             onClick={handleNextClick}
-            className="text-background rounded-full font-semibold"
+            className="mt-4 text-background rounded-full font-semibold"
             size="rounded"
             variant="default"
           >
