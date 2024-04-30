d191b9ae7f423d9a1244771e08ed09b2ee912495
Andy Terwilliger <andyterwilliger1@gmail.com>
2024-04-29 20:23:35 -0500

    Removed inline styling from span, replaced with class

--- apps/web/app/vision/page.tsx
+++ apps/web/app/vision/page.tsx
@@ -17,7 +17,7 @@ export default function Vision() {
     <div className="flex min-h-screen w-full flex-col">
       <main className="bg-background flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10 justify-between">
         <div className="mx-auto grid w-full max-w-6xl gap-2 my-24">
-          <h1 className="text-4xl font-semibold pb-10">The dance ecosystem in <span style={{color:"#00CCB6"}}>motiion</span></h1>
+          <h1 className="text-4xl font-semibold pb-10">The dance ecosystem in <span className="text-accent">motiion</span></h1>
           <p className="text-xl">Our mission is to empower dancers with innovative tools and resources to enhance their mental health, financial stability, and creative expression, fostering a vibrant and sustainable ecosystem.</p>
         </div>
         <div className="mx-auto grid w-full max-w-6xl gap-2 mb-8">
