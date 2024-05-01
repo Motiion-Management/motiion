3b3e27800574cb376ee04a8dfcdf9d353ade7717
Andy Terwilliger <andyterwilliger1@gmail.com>
2024-05-01 10:59:07 -0500

    Form fields all set up, currently logging all data to console on submit

--- apps/web/app/account_setup/splash/page.tsx
+++ apps/web/app/account_setup/splash/page.tsx
@@ -17,41 +17,144 @@ import { Input } from '@/components/ui/input'
 
 const formSchema = z.object({
   firstName: z.string().min(2, {
-    message: 'Username must be at least 2 characters.'
-  })
+    message: 'Please enter your first name.'
+  }),
+  lastName: z.string().min(2, {
+    message: 'Please enter your last name.'
+  }),
+  dob: z.string().date(),
+  gender: z.string(),
+  contact: z.string(),
+  location: z.string()
 })
 
 export default function ProfileForm() {
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      firstName: ''
+      firstName: '',
+      lastName: '',
+      dob: '',
+      gender: '',
+      contact: '',
+      location: ''
     }
   })
   function onSubmit(values: z.infer<typeof formSchema>) {
     console.log(values)
   }
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="firstName"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>First Name</FormLabel>
-              <FormControl>
-                <Input placeholder="First Name" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+    <section className="p-4">
+      <div>
+        <div className="flex justify-between">
+          <p>Account Setup</p>
+          <p>Step 1 of 3</p>
+        </div>
+
+        <h2 className="my-4">Personal Information</h2>
+      </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col items-center space-y-8"
+        >
+          <div className="flex gap-4">
+            <FormField
+              control={form.control}
+              name="firstName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>First Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="First Name" {...field} />
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="lastName"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Last Name</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Last Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div>
+            <FormField
+              control={form.control}
+              name="dob"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>DOB</FormLabel>
+                  <FormControl>
+                    <Input type="date" placeholder="Last Name" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="flex gap-4">
+            <FormField
+              control={form.control}
+              name="gender"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>I Identify As</FormLabel>
+                  <FormControl>
+                    <select className='flex flex-col items-start' {...field}>
+                      <option value="">Select</option>
+                      <option value="male">Male</option>
+                      <option value="female">Female</option>
+                      <option value="">Non-binary</option>
+
+                    </select>
+                  </FormControl>
+
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="contact"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Contact</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Phone Number" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div>
+            <FormField
+              control={form.control}
+              name="location"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Location</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Location" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </section>
   )
 }
