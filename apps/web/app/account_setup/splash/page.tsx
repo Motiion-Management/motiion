9b3c9da67c8463f828ea0c9c29a77900e7d5bb74
Jake Hebert <jacobshebert@gmail.com>
2024-05-03 00:54:55 -0500

    Updated styles for step 1 forms, all fields except calendar are finished

--- apps/web/app/account_setup/splash/page.tsx
+++ apps/web/app/account_setup/splash/page.tsx
@@ -1,9 +1,7 @@
 'use client'
 import './splash.css'
-import { useEffect, useRef } from 'react'
-import { PlaceKit } from '@placekit/autocomplete-react'
-import '@placekit/autocomplete-js/dist/placekit-autocomplete.css'
 
+import { PlaceKit } from '@placekit/autocomplete-react'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm, Controller } from 'react-hook-form'
 import { z } from 'zod'
@@ -11,7 +9,6 @@ import { Button } from '@/components/ui/button'
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -24,7 +21,16 @@ import {
   AccordionTrigger
 } from '@/components/ui/accordion'
 import { Input } from '@/components/ui/input'
-import { Ghost } from 'lucide-react'
+import { Progress } from '@/components/ui/progress'
+import { InputField } from '@/components/ui/form-fields/input'
+import { PlusCircle } from 'lucide-react'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select'
 
 const formSchema = z.object({
   firstName: z.string().min(2, {
@@ -41,7 +47,6 @@ const formSchema = z.object({
 })
 
 export default function ProfileForm() {
-  const inputRef = useRef(null)
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -62,59 +67,37 @@ export default function ProfileForm() {
     <section className="p-4">
       <div>
         <div className="flex justify-between text-sm">
-          <p >Account Setup</p>
+          <p>Account Setup</p>
           <p>Step 1 of 3</p>
         </div>
 
         <h2 className="my-4 text-xl">Personal Information</h2>
-      <div className='flex justify-center'>
-        <progress className="w-screen rounded-full" value="30" max="100"></progress>
+        <div className="flex justify-center">
+          <Progress value={30} max={100} />
         </div>
       </div>
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="flex flex-col items-center space-y-8 mt-8 w-full"
+          className="mt-8 flex w-full flex-col items-center gap-6"
         >
-          <div className="flex gap-4">
-            <FormField
-              control={form.control}
-              name="firstName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="flex justify-between items-center text-sm">First Name <span className='text-xs'>Required</span></FormLabel>
-                  <FormControl>
-                    <Input style={{backgroundColor: '#007064'}} placeholder="First Name" {...field} />
-                  </FormControl>
-
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="lastName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className='flex justify-between items-center text-sm'>Last Name <span  className='text-xs'>Required</span></FormLabel>
-                  <FormControl>
-                    <Input placeholder="Last Name" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <div>
-            <Accordion type="multiple">
-              <AccordionItem value="item-1">
-                <AccordionTrigger>+ Add Display Name</AccordionTrigger>
+          <div className="grid grid-cols-2 gap-x-4">
+            <InputField required name="firstName" placeholder="First Name" />
+            <InputField required name="lastName" placeholder="Last Name" />
+            <Accordion type="multiple" className="col-span-2 w-full">
+              <AccordionItem value="item-1" className="border-none">
+                <AccordionTrigger
+                  StartIcon={PlusCircle}
+                  startIconClassName="stroke-background fill-accent"
+                >
+                  Add Display Name
+                </AccordionTrigger>
                 <AccordionContent>
                   <FormField
                     control={form.control}
                     name="displayName"
                     render={({ field }) => (
-                      <FormItem>
+                      <FormItem className="">
                         <FormLabel></FormLabel>
                         <FormControl>
                           <Input placeholder="Display Name" {...field} />
@@ -127,86 +110,92 @@ export default function ProfileForm() {
               </AccordionItem>
             </Accordion>
           </div>
-          <div>
-            <FormField
-              control={form.control}
-              name="dob"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="flex justify-between items-center text-sm">DOB <span className='text-xs'>Required</span></FormLabel>
-                  <FormControl>
-                    <Input type="date" placeholder="Last Name" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
+          <FormField
+            control={form.control}
+            name="dob"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel className="flex items-center justify-between text-sm">
+                  DOB <span className="text-xs">Required</span>
+                </FormLabel>
+                <FormControl>
+                  <Input type="date" placeholder="mm/dd/yyyy" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <div className="flex gap-4">
             <FormField
               control={form.control}
               name="gender"
               render={({ field }) => (
                 <FormItem>
-                  <FormLabel className="flex justify-between items-center text-sm">I Identify As <span className='text-xs'>Required</span></FormLabel>
-                  <FormControl>
-                    <select className="flex flex-col items-start" {...field}>
-                      <option value="">Select</option>
-                      <option value="male">Male</option>
-                      <option value="female">Female</option>
-                      <option value="">Non-binary</option>
-                    </select>
-                  </FormControl>
-
+                  <FormLabel className="flex items-center justify-between text-sm">
+                    I identify as <span className="text-xs">Required</span>
+                  </FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger className="w-[180px]">
+                        <SelectValue placeholder="Select" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="Male">Male</SelectItem>
+                      <SelectItem value="Female">Female</SelectItem>
+                      <SelectItem value="Non-Binary">Non-Binary</SelectItem>
+                    </SelectContent>
+                  </Select>
                   <FormMessage />
                 </FormItem>
               )}
             />
-            <FormField
-              control={form.control}
-              name="contact"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="flex justify-between items-center text-sm">Contact <span className='text-xs'>Required</span></FormLabel>
-                  <FormControl>
-                    <Input placeholder="Phone Number" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          <div>
-            <Controller
-              control={form.control}
-              name="location"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel className="flex justify-between items-center text-sm">Location <span className='text-xs'>Required</span></FormLabel>
-                  <FormControl>
-                    <PlaceKit
-                      apiKey={`${process.env.NEXT_PUBLIC_PLACEKIT_KEY}`}
-                      geolocation={false}
-                      onPick={(location) => {
-                        console.log(location)
-                        field.onChange(location)
-                      }}
-                      options={{
-                        types: ['city', 'administrative'],
-                        format: {
-                          sub: (item) => `${item.city}, ${item.administrative}`,
-                          value: (item) =>
-                            `${item.city}, ${item.administrative}`
-                        }
-                      }}
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
+            <InputField
+              required
+              name="phone"
+              placeholder="Phone Number"
+              label="Contact"
             />
           </div>
-          <Button variant="default" type="submit">Continue</Button>
+          <Controller
+            control={form.control}
+            name="location"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormLabel className="flex items-center justify-between text-sm">
+                  Location <span className="text-xs">Required</span>
+                </FormLabel>
+                <FormControl>
+                  <PlaceKit
+                    apiKey={`${process.env.NEXT_PUBLIC_PLACEKIT_KEY}`}
+                    geolocation={false}
+                    className="search-root"
+                    onPick={(location) => {
+                      console.log(location)
+                      field.onChange(location)
+                    }}
+                    options={{
+                      types: ['city', 'administrative'],
+                      panel: {
+                        className: '!bg-black'
+                      },
+                      format: {
+                        sub: (item) => `${item.city}, ${item.administrative}`,
+                        value: (item) => `${item.city}, ${item.administrative}`
+                      }
+                    }}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button className="w-full" type="submit">
+            Continue
+          </Button>
         </form>
       </Form>
     </section>
