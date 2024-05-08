d517ac568498433cdf04761acd39eb3681575661
Jake Hebert <jacobshebert@gmail.com>
2024-05-08 14:36:20 -0500

    styling fixes for forms and onboarding screen

--- apps/web/app/(app)/onboarding/3/page.tsx
+++ apps/web/app/(app)/onboarding/3/page.tsx
@@ -1,31 +1,34 @@
 'use client'
 
-
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
-
-
-
 export default function Headshot() {
-  
   return (
-    <section className='flex flex-col gap-8 max-w-sm'>
-    <p className='my-8'>Upload at least one headshot to continue setting up your account. Your headshot will be viewable to the public.</p>
-     <div className = "flex justify-center">
+    <section className="flex max-w-sm flex-col gap-8">
+      <p className="my-8">
+        Upload at least one headshot to continue setting up your account. Your
+        headshot will be viewable to the public.
+      </p>
+      <div className="flex justify-center">
         <Image
           src="/images/upload-image-placeholder.png"
           alt="Upload Image Placeholder"
           width={200}
           height={200}
         />
-        </div>
-        <div className="flex justify-center">
-          <Button className="col-span-2 w-full">
-            Submit
-            <input className='opacity-0 absolute inset-0 cursor-pointer' type="file" name="" id="" />
-          </Button>
-        </div>
+      </div>
+      <div className="flex justify-center">
+        <Button className="relative col-span-2 w-full">
+          Submit
+          <input
+            className="absolute inset-0 cursor-pointer opacity-0"
+            type="file"
+            name=""
+            id=""
+          />
+        </Button>
+      </div>
     </section>
   )
 }
--- apps/web/app/(app)/onboarding/4/page.tsx
+++ apps/web/app/(app)/onboarding/4/page.tsx
@@ -44,7 +44,7 @@ export default function Onboarding1() {
     toast({
       title: 'You submitted the following values:',
       description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+        <pre className="mt-2 w-[340px] rounded-lg bg-slate-950 p-4">
           <code className="text-white">{JSON.stringify(data, null, 2)}</code>
         </pre>
       )
