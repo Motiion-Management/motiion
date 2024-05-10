2e8de0c5b627b7223ad66bbfd58653ffbc23ed39
Jake Hebert <jacobshebert@gmail.com>
2024-05-10 12:42:41 -0500

    basic pwa config setup

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,4 +1,5 @@
-import type { Metadata } from 'next'
+import type { Metadata, Viewport } from 'next'
+
 import { Manrope } from 'next/font/google'
 import './globals.css'
 import ConvexClientProvider from './ConvexClientProvider'
@@ -7,9 +8,48 @@ import { SpeedInsights } from '@vercel/speed-insights/next'
 
 const manrope = Manrope({ subsets: ['latin'] })
 
+const APP_NAME = 'Motiion'
+const APP_DEFAULT_TITLE = 'Motiion'
+const APP_TITLE_TEMPLATE = '%s - Motiion'
+const APP_DESCRIPTION = 'The dance ecosystem in motiion.'
+
 export const metadata: Metadata = {
-  title: 'Motiion',
-  description: 'Where professional dancers get hired.'
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: APP_DEFAULT_TITLE
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false
+  },
+  openGraph: {
+    type: 'website',
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE
+    },
+    description: APP_DESCRIPTION
+  },
+  twitter: {
+    card: 'summary',
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE
+    },
+    description: APP_DESCRIPTION
+  }
+}
+
+export const viewport: Viewport = {
+  themeColor: '#00CCB7'
 }
 
 export default function RootLayout({
--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,13 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
+import withSerwistInit from '@serwist/next'
+
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: 'app/sw.ts',
+  swDest: 'public/sw.js'
+})
+
+export default withSerwist({
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '*.convex.cloud', port: '' }
@@ -11,6 +17,4 @@ const nextConfig = {
     NEXT_PUBLIC_CLERK_SIGN_IN_URL: '/sign-in',
     NEXT_PUBLIC_CLERK_SIGN_UP_URL: '/sign-up'
   }
-}
-
-export default nextConfig
+})
