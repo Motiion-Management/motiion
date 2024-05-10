ca780de755084a279a0700ec19cd65102cdc3816
Andy Terwilliger <andyterwilliger1@gmail.com>
2024-05-10 13:54:40 -0500

    Carousel added, delete button added to each headshot, styles still need massaging

--- apps/web/app/(app)/onboarding/3/page.tsx
+++ apps/web/app/(app)/onboarding/3/page.tsx
@@ -6,27 +6,42 @@ import { SecondaryHeadshotUploadButton } from '@/components/ui/secondary-headsho
 import { useMutation, useQuery } from 'convex/react'
 import { api } from '@packages/backend/convex/_generated/api'
 import { Button } from '@/components/ui/button'
+import { X } from 'lucide-react'
+import {Carousel, CarouselItem, CarouselContent, CarouselApi} from '@/components/ui/carousel'
 export default function Headshot() {
   const headshots = useQuery(api.resumes.getMyHeadshots)
   const removeHeadshot = useMutation(api.resumes.removeHeadshot)
-
   const headshotsExist = headshots && headshots.length > 0
   return (
     <section className="grid h-full grid-cols-1 place-items-center gap-8">
       {headshotsExist ? (
         <div className="grid w-max grid-flow-col grid-rows-1 gap-4 justify-self-start overflow-x-auto">
           <SecondaryHeadshotUploadButton />
-          {headshots.map((headshot) => (
-            <Image
-              onClick={() => removeHeadshot({ headshotId: headshot.storageId })}
-              key={headshot.title || headshot.url}
-              src={headshot.url || ''}
-              width={100}
-              height={148}
-              className="rounded-lg object-cover hover:cursor-pointer"
-              alt={headshot.title || 'Headshot'}
-            />
+          <Carousel>
+            <CarouselContent>
+          {headshots.map((headshot, index) => (
+            <CarouselItem key={index} className="basis-auto">
+             <div key={index} className="relative h-[148px] w-[100px]"> 
+              <button
+                onClick={() =>
+                  removeHeadshot({ headshotId: headshot.storageId })
+                }
+                className="bg-input absolute relative -top-5 left-20 z-10 grid h-[48px] w-[48px] place-items-center rounded-full border"
+              >
+                <X className="stroke-white" />
+              </button>
+              <Image
+                key={headshot.title || headshot.url}
+                src={headshot.url || ''}
+                layout="fill"
+                className="rounded-lg object-cover hover:cursor-pointer"
+                alt={headshot.title || 'Headshot'}
+              />
+             </div> 
+            </CarouselItem>
           ))}
+          </CarouselContent>
+          </Carousel>
         </div>
       ) : (
         <>
