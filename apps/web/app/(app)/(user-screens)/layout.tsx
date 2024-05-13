d32346abd9298b271e890a018cc2a8e3c112d891
Jake Hebert <jacobshebert@gmail.com>
2024-05-13 18:17:14 -0500

    add pull-to-refresh function on PWA

--- apps/web/app/(app)/(user-screens)/layout.tsx
+++ apps/web/app/(app)/(user-screens)/layout.tsx
@@ -7,7 +7,7 @@ export default function UserScreensLayout({
   children
 }: UserScreensLayoutProps) {
   return (
-    <div className="grid-areas-user-screen grid-rows-user-screen grid-cols-user-screen md:grid-rows-user-screen md:grid-areas-user-screen-md grid h-full">
+    <div className="grid-areas-user-screen grid-rows-user-screen grid-cols-user-screen md:grid-rows-user-screen md:grid-areas-user-screen-md grid h-full pb-4">
       <Nav className="grid-in-nav" />
       <main className="grid-in-content grid h-full place-items-center">
         {children}
