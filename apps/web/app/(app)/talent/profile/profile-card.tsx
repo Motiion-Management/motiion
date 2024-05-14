a4687983405afc385bfc2d257958c0782ab3c601
Andy Terwilliger <andyterwilliger1@gmail.com>
2024-05-14 17:26:58 -0500

    Changed to client component, added useQuery, conditional rendering for headshots

--- apps/web/app/(app)/talent/profile/profile-card.tsx
+++ apps/web/app/(app)/talent/profile/profile-card.tsx
@@ -1,5 +1,5 @@
-import { fetchQuery } from 'convex/nextjs'
-import { getAuthToken } from '@/lib/server/utils'
+'use client'
+import { useQuery } from 'convex/react'
 import { api } from '@packages/backend/convex/_generated/api'
 import Image from 'next/image'
 import {
@@ -11,8 +11,7 @@ import { UserDoc } from '@packages/backend/convex/users'
 import { AspectRatio } from '@/components/ui/aspect-ratio'
 
 export async function ProfileCard({ user }: { user: UserDoc }) {
-  const token = await getAuthToken()
-  const headshots = await fetchQuery(api.resumes.getMyHeadshots, {}, { token })
+  const headshots = useQuery(api.resumes.getMyHeadshots)
 
   return (
     <div className="relative grid">
@@ -25,25 +24,29 @@ export async function ProfileCard({ user }: { user: UserDoc }) {
 
       <Carousel className="absolute left-0 top-0 w-full">
         <CarouselContent>
-          {headshots.map((headshot, index) => (
-            <CarouselItem key={index} className="w-full basis-auto">
-              <AspectRatio
-                key={index}
-                ratio={24 / 41}
-                className="w-full"
-                id="ar"
-              >
-                <Image
-                  key={headshot.title || headshot.url}
-                  src={headshot.url || ''}
-                  width={400}
-                  height={600}
-                  className="h-full w-full rounded-lg object-cover"
-                  alt={headshot.title || 'Headshot'}
-                />
-              </AspectRatio>
-            </CarouselItem>
-          ))}
+          {headshots && headshots.length > 0 ? (
+            headshots.map((headshot, index) => (
+              <CarouselItem key={index} className="w-full basis-auto">
+                <AspectRatio
+                  key={index}
+                  ratio={24 / 41}
+                  className="w-full"
+                  id="ar"
+                >
+                  <Image
+                    key={headshot.title || headshot.url}
+                    src={headshot.url || ''}
+                    width={400}
+                    height={600}
+                    className="h-full w-full rounded-lg object-cover"
+                    alt={headshot.title || 'Headshot'}
+                  />
+                </AspectRatio>
+              </CarouselItem>
+            ))
+          ) : (
+            <div>No headshots available</div> // Fallback UI when there are no headshots
+          )}
         </CarouselContent>
       </Carousel>
     </div>
