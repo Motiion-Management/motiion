ac57aeda04947e5622e67d2fc544932d780a791e
Jake Hebert <jacobshebert@gmail.com>
2024-05-14 11:50:17 -0500

    add navigation to profile view button

--- apps/web/app/(app)/talent/@title/profile/page.tsx
+++ apps/web/app/(app)/talent/@title/profile/page.tsx
@@ -1,13 +1,17 @@
+'use client'
 import { Badge } from '@/components/ui/badge'
 import { Header } from '@/components/ui/header'
 import { Eye } from 'lucide-react'
+import { useRouter } from 'next/navigation'
 
 export default function TitleSlot() {
+  const router = useRouter()
+  const goToEdit = () => router.push('/talent/profile/edit')
   return (
     <Header
       title="Profile"
       actionSlot={
-        <Badge className="flex gap-2">
+        <Badge className="flex gap-2" onClick={goToEdit}>
           Preview <Eye />
         </Badge>
       }
