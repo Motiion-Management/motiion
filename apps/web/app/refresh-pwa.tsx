3943ead3378ac7c58a4fb4149a3507707cf82e3e
Jake Hebert <jacobshebert@gmail.com>
2024-05-13 20:14:25 -0500

    update refresh code to allow scrolling of inner elements

--- apps/web/app/refresh-pwa.tsx
+++ apps/web/app/refresh-pwa.tsx
@@ -7,10 +7,31 @@ export function RefreshPWA() {
     typeof window !== 'undefined' &&
     window.matchMedia('(display-mode: standalone)').matches
   ) {
-    PullToRefresh.init({
-      onRefresh() {
-        window.location.reload()
+    let shouldRefresh = true
+    document.body.addEventListener('touchstart', (ev) => {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      let el = ev.target as any
+      while (el?.parentNode && el !== document.body) {
+        if (el.scrollTop > 0) {
+          shouldRefresh = false
+        }
+
+        // Check if element has role=dialog
+        if (el.getAttribute('role') === 'dialog') {
+          shouldRefresh = false
+          break
+        }
+        el = el.parentNode as HTMLElement
       }
+    })
+
+    document.body.addEventListener('touchend', () => {
+      shouldRefresh = true
+    })
+
+    PullToRefresh.init({
+      distThreshold: 120,
+      shouldPullToRefresh: () => !window.scrollY && shouldRefresh
     })
   }
   return null
