6faee06fce8eab86348b55916e7c0118a198d9c3
Jake Hebert <jacobshebert@gmail.com>
2024-05-15 13:45:34 -0500

    remove commented code

--- apps/web/app/(app)/talent/@title/profile/preview/page.tsx
+++ apps/web/app/(app)/talent/@title/profile/preview/page.tsx
@@ -1,13 +1,9 @@
-// 'use client'
 import { Badge } from '@/components/ui/badge'
 import { Header } from '@/components/ui/header'
 import { Eye } from 'lucide-react'
 import Link from 'next/link'
-// import { useRouter } from 'next/navigation'
 
 export default function TitleSlot() {
-  // const router = useRouter()
-  // const goToEdit = () => router.push('/talent/profile/edit')
   return (
     <Header
       title="Profile"
