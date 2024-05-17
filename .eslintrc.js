b3df89df90069ed8365a5b21807ee3a6b87dbf06
Jake Hebert <jacobshebert@gmail.com>
2024-05-17 15:01:29 -0500

    update settings to allow builds

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,16 @@
 module.exports = {
-  extends: ['next/core-web-vitals', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
   settings: {
     next: {
       rootDir: 'apps/next/'
     }
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off'
   },
   root: true
 }
--- apps/web/app/(app)/talent/profile/page.tsx
+++ apps/web/app/(app)/talent/profile/page.tsx
@@ -1,9 +1,4 @@
-import { me } from '@/lib/server/users'
-// import { Header } from '@/components/ui/header'
-// import { Circle } from 'lucide-react'
-
 export default async function ProfilePage() {
-  const user = await me()
   return (
     <div className="flex w-full flex-col gap-2">
       {/* <Header title="Profile" actionSlot={<Circle />}> */}
