88489d9dab09629d93200d51dcf64ccfeb757fa2
Jake Hebert <jacobshebert@gmail.com>
2024-05-29 13:28:33 -0700

    Feature/mo 34 add error boundary (#16)
    * add error boundary
    
    * update carousel to have loading state
    
    * add key to skeleton array

--- apps/web/app/(app)/layout.tsx
+++ apps/web/app/(app)/layout.tsx
@@ -1,18 +1,13 @@
-import { Toaster } from 'sonner'
-
 export default async function AppLayout({
   children
 }: {
   children: React.ReactNode
 }) {
   return (
-    <>
-      <div className="touch:bg-card bg-primary/10 grid h-dvh w-screen place-items-center">
-        <div className="shadow-card bg-card h-full max-h-dvh min-h-max w-full overflow-clip overflow-y-auto py-5 shadow-md md:max-h-[850px] md:max-w-screen-sm md:rounded-2xl">
-          {children}
-        </div>
+    <div className="touch:bg-card bg-primary/10 grid h-dvh w-screen place-items-center">
+      <div className="shadow-card bg-card h-full max-h-dvh min-h-max w-full overflow-clip overflow-y-auto py-5 shadow-md md:max-h-[850px] md:max-w-screen-sm md:rounded-2xl">
+        {children}
       </div>
-      <Toaster richColors />
-    </>
+    </div>
   )
 }
---
+++ apps/web/app/(app)/onboarding/3/button.tsx
@@ -0,0 +1,37 @@
+'use client'
+import { Button } from '@/components/ui/button'
+import { HeadshotUploadButton } from '@/components/ui/headshot-upload-button'
+import { useMutation, useQuery } from 'convex/react'
+import { useState } from 'react'
+import { ONBOARDING_STEPS } from '@packages/backend/convex/users'
+import { api } from '@packages/backend/convex/_generated/api'
+
+export function BottomButton() {
+  const headshots = useQuery(api.resumes.getMyHeadshots)
+  const headshotsExist = headshots && headshots.length > 0
+  const updateMyUser = useMutation(api.users.updateMyUser)
+  const user = useQuery(api.users.getMyUser)
+  const [loading, setLoading] = useState(false)
+
+  async function nextStep() {
+    setLoading(true)
+    await updateMyUser({
+      id: user!._id,
+      patch: {
+        onboardingStep: ONBOARDING_STEPS.RESUME
+      }
+    })
+  }
+
+  return (
+    <>
+      {headshotsExist ? (
+        <Button onClick={nextStep} className="w-full" loading={loading}>
+          Continue
+        </Button>
+      ) : (
+        <HeadshotUploadButton className="w-full" />
+      )}
+    </>
+  )
+}
