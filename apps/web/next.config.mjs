89706f6988128c8ff591a7996f9c9b6eac270a00
Jake Hebert <jacobshebert@gmail.com>
2024-06-03 14:15:25 -0500

    opt out of ppr

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -9,9 +9,9 @@ const withSerwist = withSerwistInit({
 })
 
 export default withSerwist({
-  experimental: {
-    ppr: true
-  },
+  // experimental: {
+  //   ppr: true
+  // },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '*.convex.cloud', port: '' }
