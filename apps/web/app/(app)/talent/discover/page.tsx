699aa88bc8e0aab1d9bc2a8b63210eab48db3191
Andy Terwilliger <andyterwilliger1@gmail.com>
2024-06-03 22:53:20 -0400

    basic search bar styles

--- apps/web/app/(app)/talent/discover/page.tsx
+++ apps/web/app/(app)/talent/discover/page.tsx
@@ -3,7 +3,8 @@ import { getAuthToken } from '@/lib/server/utils'
 import { api } from '@packages/backend/convex/_generated/api'
 import Link from 'next/link'
 import { Button } from '@/components/ui/button'
-
+import { Search } from 'lucide-react'
+import { Input } from '@/components/ui/input'
 export default async function DiscoverPage() {
   const token = await getAuthToken()
   const users = await fetchQuery(
@@ -15,7 +16,7 @@ export default async function DiscoverPage() {
   return (
     <div className="flex flex-col gap-2">
       <h1>Discover</h1>
-      {users.page.map((user) => (
+      {/* {users.page.map((user) => (
         <Link
           key={user._id}
           className="text-link-lg"
@@ -25,7 +26,15 @@ export default async function DiscoverPage() {
             {user.firstName} {user.lastName}
           </Button>
         </Link>
-      ))}
+      ))} */}
+       <div className="relative">
+            <Search className="absolute left-2.5 top-2.5 w-4" />
+            <Input
+              type="search"
+              placeholder="Search for dance artists."
+              className="pl-8 sm:w-[300px] md:w-[200px] lg:w-[300px] text-primary"
+            />
+    </div>
     </div>
   )
 }
