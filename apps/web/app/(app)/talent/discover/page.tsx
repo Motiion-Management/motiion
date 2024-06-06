6b7516b8a51d9b5b920399ff0260fb7cb7d938f7
Andy Terwilliger <andyterwilliger1@gmail.com>
2024-06-06 01:02:41 -0400

    Ellipsis breadcrumbs and styles

--- apps/web/app/(app)/talent/discover/page.tsx
+++ apps/web/app/(app)/talent/discover/page.tsx
@@ -1,22 +1,11 @@
 'use client'
-import { fetchQuery } from 'convex/nextjs'
-import { useQuery, useQueries } from 'convex/react'
-import { getAuthToken } from '@/lib/server/utils'
-import {
-  searchFirstNameUsers,
-  searchLastNameUsers
-} from '@packages/backend/convex/users'
+import { useQuery } from 'convex/react'
 import { api } from '@packages/backend/convex/_generated/api'
-import Link from 'next/link'
 import Image from 'next/image'
-import { Button } from '@/components/ui/button'
 import { Search } from 'lucide-react'
 import { Input } from '@/components/ui/input'
 import { useState } from 'react'
-import {
-  getPublicResume,
-  getUserHeadshots
-} from '@packages/backend/convex/resumes'
+import  {BreadcrumbEllipsis}  from '@/components/ui/breadcrumb'
 export default function DiscoverPage() {
   const [searchQuery, setSearchQuery] = useState('')
   const firstNameResults =
@@ -42,20 +31,29 @@ export default function DiscoverPage() {
           onChange={handleSearch}
         />
       </div>
-      <div>
+      <div className="mt-5">
         {searchResults.map((user, index) => (
-          <div key={index}>
-            {/* Render your result here. This is just a placeholder. */}
-            <h2 className="text-lg font-bold">
-              {user.firstName} {user.lastName}
-            </h2>
-            <p> {user.representation}</p>
+          <div className="flex items-center justify-between gap-3 my-5" key={index}>
+            <div className="flex items-center gap-3">
             <Image
+              className="rounded-full h-10 w-10 object-cover"
               src={user.headshot}
               alt={`${user.firstName} ${user.lastName}`}
               width={100}
               height={100}
             />
+          <div>
+            {/* Render your result here. This is just a placeholder. */}
+            <h2 className="text-lg font-bold">
+              {user.firstName} {user.lastName}
+            </h2>
+            <p className="text-[#1c1d2099]"> {user.representation}</p>
+          </div>
+          </div>
+
+          <div>
+            <BreadcrumbEllipsis />
+          </div>
           </div>
         ))}
       </div>
