1d1f991fbb9d1ff4e0b5dd09b9594682d2b77137
Jake Hebert <jacobshebert@gmail.com>
2024-06-11 01:24:37 -0500

    migrate talent section

--- apps/web/app/(app)/talent/[userId]/page.tsx
+++ apps/web/app/(app)/talent/[userId]/page.tsx
@@ -3,7 +3,6 @@ import { ScrollCollapse } from '@/components/features/profile/scroll-collapse'
 import { ProfileCard } from '@/components/features/profile/profile-card'
 import { TabSection } from '@/components/features/profile/tab-section'
 import { getPublicUser } from '@/lib/server/users'
-import { getPublicResume } from '@/lib/server/resumes'
 
 export default async function UserProfilePage({
   params
@@ -11,13 +10,12 @@ export default async function UserProfilePage({
   params: { userId: Id<'users'> }
 }) {
   const user = await getPublicUser(params.userId)
-  const resume = await getPublicResume(params.userId)
   return (
     <div className="flex flex-col gap-2 ">
       <ScrollCollapse collapsedTitle={`${user?.firstName} ${user?.lastName}`}>
-        <ProfileCard user={user} resume={resume} />
+        <ProfileCard user={user} />
       </ScrollCollapse>
-      <TabSection user={user} resume={resume} />
+      <TabSection user={user} />
     </div>
   )
 }
--- apps/web/app/(app)/talent/discover/search-results.tsx
+++ apps/web/app/(app)/talent/discover/search-results.tsx
@@ -5,13 +5,10 @@ import { api } from '@packages/backend/convex/_generated/api'
 import { BreadcrumbEllipsis } from '@/components/ui/breadcrumb'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { FC } from 'react'
+import { AgencyName } from '@/components/features/agencies/agency-name'
 
 export const SearchResults: FC<{ searchQuery: string }> = ({ searchQuery }) => {
-  const searchResults =
-    useQuery(api.users.searchUsersByName, { query: searchQuery }) || []
-  // const lastNameResults =
-  //   useQuery(api.users.searchLastNameUsers, { query: searchQuery }) || []
-  // const searchResults = [...firstNameResults, ...lastNameResults]
+  const searchResults = useQuery(api.users.search, { query: searchQuery }) || []
 
   return (
     <div className="mt-5">
@@ -23,8 +20,8 @@ export const SearchResults: FC<{ searchQuery: string }> = ({ searchQuery }) => {
           <div className="flex items-center gap-3">
             <Avatar>
               <AvatarImage
-                src={user.headshot || undefined}
-                alt={`${user.firstName} ${user.lastName}`}
+                src={user.headshot?.url || undefined}
+                alt={`${user.fullName}`}
               />
 
               <AvatarFallback>
@@ -35,10 +32,10 @@ export const SearchResults: FC<{ searchQuery: string }> = ({ searchQuery }) => {
 
             <div>
               {/* Render your result here. This is just a placeholder. */}
-              <h2 className="text-lg font-bold">
-                {user.firstName} {user.lastName}
-              </h2>
-              <p className="text-[#1c1d2099]"> {user.representation}</p>
+              <h2 className="text-lg font-bold">{user.fullName}</h2>
+              {user.representation?.agencyId && (
+                <AgencyName id={user.representation.agencyId} />
+              )}
             </div>
           </div>
 
