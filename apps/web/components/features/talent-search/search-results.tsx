35b436a2d83e54cd9df76587377150164f9cc8c7
Jake Hebert <jacobshebert@gmail.com>
2024-06-12 23:42:49 -0500

    fix search result jank (more, avatars were slowing it all down)

--- apps/web/components/features/talent-search/search-results.tsx
+++ apps/web/components/features/talent-search/search-results.tsx
@@ -1,46 +1,53 @@
 'use client'
 import { useStableQuery } from '@/lib/hooks/use-stable-query'
 import { api } from '@packages/backend/convex/_generated/api'
+import { motion } from 'framer-motion'
 
 import { BreadcrumbEllipsis } from '@/components/ui/breadcrumb'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { FC } from 'react'
+import Image from 'next/image'
+import { Initials } from './initials'
 
 export const SearchResults: FC<{ searchQuery: string }> = ({ searchQuery }) => {
   const searchResults =
     useStableQuery(api.users.search, { query: searchQuery }) || []
 
   return (
-    <div className="mt-5 grid">
-      {searchResults.map((user) => (
-        <div
-          className="my-5 flex items-center justify-between gap-3"
-          key={user._id}
-        >
-          <div className="flex items-center gap-3">
-            <Avatar>
-              <AvatarImage
-                src={user.headshot?.url || undefined}
-                alt={`${user.fullName}`}
-              />
+    <motion.div
+      className="bg-background/60 absolute left-0 z-10 -ml-4 mt-8 w-screen p-4"
+      animate={{ opacity: searchQuery ? 1 : 0 }}
+    >
+      <div className="mt-5 grid">
+        {searchResults.map((user) => (
+          <div
+            className="my-5 flex items-center justify-between gap-3"
+            key={user._id}
+          >
+            <div className="flex items-center gap-3">
+              {user.headshot?.url ? (
+                <Image
+                  width={40}
+                  height={40}
+                  className="aspect-square rounded-full object-cover"
+                  src={user.headshot?.url || ''}
+                  alt={`${user.fullName}`}
+                />
+              ) : (
+                <Initials firstName={user.firstName} lastName={user.lastName} />
+              )}
 
-              <AvatarFallback>
-                {user.firstName?.[0]}
-                {user.lastName?.slice(0, 1)}
-              </AvatarFallback>
-            </Avatar>
+              <div>
+                <h2 className="text-lg font-bold">{user.fullName}</h2>
+                {user.representationName}
+              </div>
+            </div>
 
             <div>
-              <h2 className="text-lg font-bold">{user.fullName}</h2>
-              {user.representationName}
+              <BreadcrumbEllipsis />
             </div>
           </div>
-
-          <div>
-            <BreadcrumbEllipsis />
-          </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </motion.div>
   )
 }
--- apps/web/components/features/talent-search/search.tsx
+++ apps/web/components/features/talent-search/search.tsx
@@ -1,10 +1,9 @@
 'use client'
 import { Search } from 'lucide-react'
 import { Input } from '@/components/ui/input'
-import { ChangeEvent, useEffect, useMemo, useState } from 'react'
+import { ChangeEvent, useCallback, useState } from 'react'
 import debounce from 'lodash.debounce'
 import { SearchResults } from './search-results'
-import { motion } from 'framer-motion'
 
 export function TalentSearch() {
   const [searchQuery, setSearchQuery] = useState('')
@@ -13,33 +12,21 @@ export function TalentSearch() {
     setSearchQuery(e.target.value)
   }
 
-  const debouncedSearch = useMemo(() => {
-    return debounce(handleSearch, 300)
+  const debouncedSearch = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    return debounce(handleSearch, 300)(e)
   }, [])
-
-  useEffect(() => {
-    return () => {
-      debouncedSearch.cancel()
-    }
-  }, [debouncedSearch])
 
   return (
     <div className="relative w-full">
       <div className="relative w-full">
         <Search className="absolute left-2.5 top-2.5 w-4" />
         <Input
-          type="text"
           placeholder="Search for dance artists."
           className="text-primary pl-8"
           onChange={debouncedSearch}
         />
       </div>
-      <motion.div
-        className="bg-background/60 absolute left-0 z-10 -ml-4 mt-8 w-screen p-4"
-        animate={{ opacity: searchQuery ? 1 : 0 }}
-      >
-        <SearchResults searchQuery={searchQuery} />
-      </motion.div>
+      <SearchResults searchQuery={searchQuery} />
     </div>
   )
 }
