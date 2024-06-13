62a7bf1f72223a0f1916e2c3096b7c1662d0b1a9
Andy Terwilliger <andyterwilliger1@gmail.com>
2024-06-12 21:53:13 -0500

    Settings tabs added, settings header deleted, most of old settings placeholders removed

--- apps/web/app/(app)/talent/settings/page.tsx
+++ apps/web/app/(app)/talent/settings/page.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-
+import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import {
   Card,
@@ -12,75 +12,42 @@ import {
 import { Checkbox } from '@/components/ui/checkbox'
 import { Input } from '@/components/ui/input'
 import { Header } from './header'
-
+import UserIcon from '@/public/UserIcon.svg'
+import HelpIcon from '@/public/Help.svg'
+import LockIcon from '@/public/Lock.svg'
+import { SettingsTabs } from './settings-tabs'
 export default function Dashboard() {
   return (
     <div className="flex min-h-screen w-full flex-col">
-      <Header />
       <main className="bg-background flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10">
         <div className="mx-auto grid w-full max-w-6xl gap-2">
           <h1 className="text-3xl font-semibold">Settings</h1>
         </div>
-        <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-          <nav
-            className="text-muted-foreground grid gap-4 text-sm"
-            x-chunk="dashboard-04-chunk-0"
-          >
-            <Link href="#" className="text-primary font-semibold">
-              General
-            </Link>
-            <Link href="#">Security</Link>
-            <Link href="#">Integrations</Link>
-            <Link href="#">Support</Link>
-            <Link href="#">Organizations</Link>
-            <Link href="#">Advanced</Link>
-          </nav>
+        <div className="mx-auto grid w-full max-w-6xl items-start gap-6">
+          <SettingsTabs
+           links={[
+            {
+              href: '/settings/account',
+              header: 'Account',
+              subtext: 'Update your personal and account info',
+              iconPath: UserIcon
+            },
+            {
+              href: '/settings/support',
+              header: 'Support',
+              subtext: 'Contact us for any issues you may experience',
+              iconPath: HelpIcon
+            },
+            {
+              href: '/settings/privacy',
+              header: 'Data & Privacy',
+              subtext: 'Update your account information',
+              iconPath: LockIcon
+            }
+          ]}
+          />
           <div className="grid gap-6">
-            <Card x-chunk="dashboard-04-chunk-1">
-              <CardHeader>
-                <CardTitle>Store Name</CardTitle>
-                <CardDescription>
-                  Used to identify your store in the marketplace.
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <form>
-                  <Input placeholder="Store Name" />
-                </form>
-              </CardContent>
-              <CardFooter className="border-t px-6 py-4">
-                <Button>Save</Button>
-              </CardFooter>
-            </Card>
-            <Card x-chunk="dashboard-04-chunk-2">
-              <CardHeader>
-                <CardTitle>Plugins Directory</CardTitle>
-                <CardDescription>
-                  The directory within your project, in which your plugins are
-                  located.
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <form className="flex flex-col gap-4">
-                  <Input
-                    placeholder="Project Name"
-                    defaultValue="/content/plugins"
-                  />
-                  <div className="flex items-center space-x-2">
-                    <Checkbox id="include" defaultChecked />
-                    <label
-                      htmlFor="include"
-                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                    >
-                      Allow administrators to change the directory.
-                    </label>
-                  </div>
-                </form>
-              </CardContent>
-              <CardFooter className="border-t px-6 py-4">
-                <Button>Save</Button>
-              </CardFooter>
-            </Card>
+           
           </div>
         </div>
       </main>
