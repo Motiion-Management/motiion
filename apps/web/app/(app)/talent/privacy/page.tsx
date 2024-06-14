810d1bbd42d45fb98dabe952ef667d54755036a0
Andy Terwilliger <andyterwilliger1@gmail.com>
2024-06-13 23:11:29 -0500

    privacy page built out

--- apps/web/app/(app)/talent/privacy/page.tsx
+++ apps/web/app/(app)/talent/privacy/page.tsx
@@ -1,5 +1,21 @@
-export default function Privacy(){
-  return(
-   <h1>Privacy Page</h1>
+import { Button } from '@/components/ui/button'
+export default function Privacy() {
+  return (
+    <div className="flex w-full flex-col overflow-hidden">
+      <main className="bg-background flex justify-start flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10">
+      <div className="mx-auto w-full max-w-6xl gap-2 pb-5">
+          <h1 className="text-2xl font-semibold">Data and Privacy</h1>
+          <p className="text-xs uppercase">Settings</p>
+        </div>
+        <div className="mx-auto grid w-full max-w-6xl items-start gap-80">
+          <div className="grid gap-6">
+            <h4 className='text-lg font-semibold'>Data Practices</h4>
+            <p>Motiion collects information from you in order to provide our service. This includes your name, email, and phone number.</p>
+          </div>
+
+            <Button>Terms and Conditions</Button>
+        </div>
+      </main>
+    </div>
   )
 }
