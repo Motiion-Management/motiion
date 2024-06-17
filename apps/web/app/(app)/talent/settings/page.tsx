37736c68862c3dddd8a7db760cf317b15d882e13
Andy Terwilliger <andyterwilliger1@gmail.com>
2024-06-17 09:42:12 -0500

    Popover added for sign out, clerk signOut function added

--- apps/web/app/(app)/talent/settings/page.tsx
+++ apps/web/app/(app)/talent/settings/page.tsx
@@ -4,16 +4,12 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { Button } from '@/components/ui/button'
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle
-} from '@/components/ui/card'
-import { Checkbox } from '@/components/ui/checkbox'
-import { Input } from '@/components/ui/input'
-import { Header } from './header'
+  Popover,
+  PopoverContent,
+  PopoverTrigger
+} from '@/components/ui/popover'
+import { Card, CardContent } from '@/components/ui/card'
+
 import UserIcon from '@/public/UserIcon.svg'
 import HelpIcon from '@/public/Help.svg'
 import LockIcon from '@/public/Lock.svg'
@@ -24,17 +20,28 @@ import { BreadcrumbEllipsis } from '@/components/ui/breadcrumb'
 import CheckIcon from '@/public/SettingsCheck.svg'
 import NotCompleteIcon from '@/public/SettingsCheckNotComplete.svg'
 import { ChevronRight } from 'lucide-react'
+import { useClerk } from '@clerk/clerk-react'
 export default function Dashboard() {
   const user = useQuery(api.users.getMyUser)
   const userResumes = user?.resume?.uploads
   const userHeadshots = user?.headshots
   const onboardingStep = user?.onboardingStep
+  const { signOut } = useClerk()
   return (
     <div className="flex min-h-screen w-full flex-col">
       <main className="bg-background flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 p-4 md:gap-8 md:p-10">
         <div className="mx-auto  flex w-full max-w-6xl items-center justify-between gap-2">
           <h1 className="text-3xl font-semibold">Settings</h1>
-          <BreadcrumbEllipsis />
+          <Popover>
+            <PopoverTrigger>
+              <BreadcrumbEllipsis />
+            </PopoverTrigger>
+            <PopoverContent>
+              <div className='flex justify-center'>
+                <Button onClick={signOut}>Sign Out</Button>
+              </div>
+            </PopoverContent>
+          </Popover>
         </div>
         <div className="mx-auto grid w-full max-w-6xl items-start gap-6">
           <Card>
@@ -45,7 +52,7 @@ export default function Dashboard() {
                   <p className="text-secondary text-3xl font-semibold">250</p>
                 </div>
                 <div className="pl-10">
-                  <ul className='pb-5'>
+                  <ul className="pb-5">
                     <li className="flex gap-1 pb-1">
                       <Image
                         width={20}
@@ -101,7 +108,10 @@ export default function Dashboard() {
                       Attend an event
                     </li>
                   </ul>
-                 <a className='flex font-semibold pointer'>Manage Points<ChevronRight /></a>
+                  <a className="pointer flex font-semibold">
+                    Manage Points
+                    <ChevronRight />
+                  </a>
                 </div>
               </div>
             </CardContent>
