d0d7eb252ece32788d7b87e155f84b6b132f508c
Jake Hebert <jacobshebert@gmail.com>
2024-06-19 16:28:33 -0500

    add headshot interactions

--- apps/web/components/features/headshots.tsx
+++ apps/web/components/features/headshots.tsx
@@ -7,6 +7,9 @@ import Image, { StaticImageData } from 'next/image'
 import { X } from 'lucide-react'
 import { Skeleton } from '../ui/skeleton'
 import { CarouselItem, CarouselContent } from '@/components/ui/carousel'
+import { Dialog, DialogTrigger } from '@/components/ui/dialog'
+import { AspectRatio } from '../ui/aspect-ratio'
+import { HeadshotModalContent } from './headshot-modal'
 
 export function HeadshotsCarouselContent({
   preloadedHeadshots,
@@ -21,40 +24,50 @@ export function HeadshotsCarouselContent({
   const removeHeadshot = useMutation(api.users.headshots.removeHeadshot)
 
   return (
-    <CarouselContent className="flex gap-2">
-      {headshots && headshots.length > 0 && headshots.length < 5 && (
-        <ItemComponent className="basis-auto">
-          <HeadshotUploadSquare />
-        </ItemComponent>
-      )}
-      {headshots.map((headshot, index) => (
-        <ItemComponent
-          key={index}
-          className="relative h-[148px] w-[100px] basis-auto "
-        >
-          <div className="bg-primary/50 absolute left-0 top-0 z-0 h-full w-full animate-pulse rounded-lg" />
-          {onboarding && (
-            <X
-              onClick={() => removeHeadshot({ headshotId: headshot.storageId })}
-              className="bg-input absolute -right-3 -top-3 z-10 grid place-items-center rounded-full border stroke-white p-[7px] hover:cursor-pointer"
-              strokeWidth={4}
-              size={32}
-            />
-          )}
-          <Image
-            data-loaded="false"
-            onLoad={(event) => {
-              event.currentTarget.setAttribute('data-loaded', 'true')
-            }}
-            className="rounded-lg object-cover "
-            key={headshot.title || headshot.url}
-            src={headshot.url || ''}
-            layout="fill"
-            alt={headshot.title || 'Headshot'}
-          />
-        </ItemComponent>
-      ))}
-    </CarouselContent>
+    <Dialog>
+      <CarouselContent className="flex gap-2">
+        {headshots && headshots.length > 0 && headshots.length < 5 && (
+          <ItemComponent className="basis-auto">
+            <HeadshotUploadSquare />
+          </ItemComponent>
+        )}
+        {headshots.map((headshot, index) => (
+          <ItemComponent
+            key={index}
+            className="relative h-[148px] w-[100px] basis-auto p-0 "
+          >
+            <AspectRatio ratio={100 / 148} className="relative h-full w-full">
+              <div className="bg-primary/50 absolute left-0 top-0 z-0 h-full w-full animate-pulse rounded-lg" />
+              {onboarding && (
+                <X
+                  onClick={() =>
+                    removeHeadshot({ headshotId: headshot.storageId })
+                  }
+                  className="bg-input absolute -right-3 -top-3 z-10 grid place-items-center rounded-full border stroke-white p-[7px] hover:cursor-pointer"
+                  strokeWidth={4}
+                  size={32}
+                />
+              )}
+
+              <DialogTrigger asChild>
+                <Image
+                  data-loaded="false"
+                  onLoad={(event) => {
+                    event.currentTarget.setAttribute('data-loaded', 'true')
+                  }}
+                  className="rounded-lg object-cover"
+                  key={headshot.title || headshot.url}
+                  src={headshot.url || ''}
+                  layout="fill"
+                  alt={headshot.title || 'Headshot'}
+                />
+              </DialogTrigger>
+            </AspectRatio>
+          </ItemComponent>
+        ))}
+      </CarouselContent>
+      <HeadshotModalContent preloadedHeadshots={preloadedHeadshots} />
+    </Dialog>
   )
 }
 
