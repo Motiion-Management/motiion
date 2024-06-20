1811a2d085ed87c9d5dcd13fae998dadb811a787
Jake Hebert <jacobshebert@gmail.com>
2024-06-19 22:59:35 -0500

    fix video embed in resume entries

--- apps/web/app/(app)/talent/[userId]/[experience]/video-or-link.tsx
+++ apps/web/app/(app)/talent/[userId]/[experience]/video-or-link.tsx
@@ -1,27 +1,11 @@
 'use client'
-import ReactPlayer from 'react-player'
-import Link from 'next/link'
-import { LinkIcon } from 'lucide-react'
-import { Separator } from '@/components/ui/separator'
+// import Link from 'next/link'
+// import { LinkIcon } from 'lucide-react'
 import { FC } from 'react'
+import { Video } from '@/components/ui/video'
 
 export const VideoOrLink: FC<{
   href?: string
 }> = ({ href }) => {
-  return (
-    href && (
-      <>
-        <ReactPlayer
-          lazy
-          src={href}
-          fallback={
-            <Link href={href}>
-              <LinkIcon size={24} />
-            </Link>
-          }
-        />
-        <Separator />
-      </>
-    )
-  )
+  return href && <Video url={href} />
 }
