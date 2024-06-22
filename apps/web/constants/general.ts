e74de96a40f93094e12dc1d78c56494eb7f98695
Jake Hebert <jacobshebert@gmail.com>
2024-06-22 16:56:55 -0500

    add https prefix

--- apps/web/constants/general.ts
+++ apps/web/constants/general.ts
@@ -1,4 +1,4 @@
 export const domain =
   process.env.NODE_ENV === 'production'
-    ? process.env.VERCEL_PROJECT_PRODUCTION_URL!
+    ? 'https://' + process.env.VERCEL_PROJECT_PRODUCTION_URL!
     : 'http://localhost:3000'
