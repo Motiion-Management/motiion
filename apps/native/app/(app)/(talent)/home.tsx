9ec4c78079aa92a8866d19323f44f09cb52b621d
Jake Hebert <jacobshebert@gmail.com>
2024-08-04 17:51:33 -0700

    feat: mobile app auth guard is working, need to fully test on better wifi

---
+++ apps/native/app/(app)/(talent)/home.tsx
@@ -0,0 +1,5 @@
+import WebView from '@/components/webview'
+
+export default function Home() {
+  return <WebView path="/talent/home" />
+}
--- apps/native/app/_layout.tsx
+++ apps/native/app/_layout.tsx
@@ -1,31 +1,33 @@
 import { Stack } from 'expo-router'
-import * as SecureStore from 'expo-secure-store'
-import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
-
-const tokenCache = {
-  async getToken(key: string) {
-    try {
-      const item = await SecureStore.getItemAsync(key)
-      if (item) {
-        console.log(`${key} was used 🔐 \n`)
-      } else {
-        console.log('No values stored under key: ' + key)
-      }
-      return item
-    } catch (error) {
-      console.error('SecureStore get item error: ', error)
-      await SecureStore.deleteItemAsync(key)
-      return null
-    }
-  },
-  async saveToken(key: string, value: string) {
-    try {
-      return SecureStore.setItemAsync(key, value)
-    } catch (err) {
-      return
-    }
-  }
-}
+// import * as SecureStore from 'expo-secure-store'
+// import { ClerkProvider, ClerkLoaded } from '@clerk/clerk-expo'
+import { StatusBar, Platform, View } from 'react-native'
+import ConvexClientProvider from '@/ConvexClientProvider'
+//
+// const tokenCache = {
+//   async getToken(key: string) {
+//     try {
+//       const item = await SecureStore.getItemAsync(key)
+//       if (item) {
+//         console.log(`${key} was used 🔐 \n`)
+//       } else {
+//         console.log('No values stored under key: ' + key)
+//       }
+//       return item
+//     } catch (error) {
+//       console.error('SecureStore get item error: ', error)
+//       await SecureStore.deleteItemAsync(key)
+//       return null
+//     }
+//   },
+//   async saveToken(key: string, value: string) {
+//     try {
+//       return SecureStore.setItemAsync(key, value)
+//     } catch (err) {
+//       return
+//     }
+//   }
+// }
 
 const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
 
@@ -35,17 +37,25 @@ if (!publishableKey) {
   )
 }
 
+const STATUS_BAR_HEIGHT = Platform.OS === 'ios' ? 50 : StatusBar.currentHeight
+
+function Header() {
+  return (
+    <View style={{ height: STATUS_BAR_HEIGHT }}>
+      <StatusBar translucent />
+    </View>
+  )
+}
+
 export default function RootLayout() {
   return (
-    <ClerkProvider tokenCache={tokenCache} publishableKey={publishableKey}>
-      <ClerkLoaded>
-        <Stack>
-          <Stack.Screen name="(app)" />
-          <Stack.Screen name="sign-in" />
-          <Stack.Screen name="sign-up" />
-          <Stack.Screen name="index" />
-        </Stack>
-      </ClerkLoaded>
-    </ClerkProvider>
+    <ConvexClientProvider>
+      <Stack screenOptions={{ header: Header }}>
+        <Stack.Screen name="(app)" />
+        <Stack.Screen name="sign-in" />
+        <Stack.Screen name="sign-up" />
+        <Stack.Screen name="index" />
+      </Stack>
+    </ConvexClientProvider>
   )
 }
