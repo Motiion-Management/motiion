163d91610f6d39dc479a6e7b1513153c4f874a06
panzacoder <jake@plfx.studio>
2024-12-01 18:48:34 -0600

    chore: update metro config and set hoisting for node_linker

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -1,12 +1,26 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
-
-  const { withNativeWind } = require("nativewind/metro");
-
+const { withNativeWind } = require('nativewind/metro');
+const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
+
+// Find the project and workspace directories
 // eslint-disable-next-line no-undef
-const config = getDefaultConfig(__dirname);
+const projectRoot = __dirname;
+// This can be replaced with `find-yarn-workspace-root`
+const workspaceRoot = path.resolve(projectRoot, '../..');
 
+const config = getDefaultConfig(projectRoot);
 
-  module.exports = withNativeWind(config, { input: "./global.css", inlineRem: 16 });
+// 1. Watch all files within the monorepo
+config.watchFolders = [workspaceRoot];
+// 2. Let Metro know where to resolve packages and in what order
+config.resolver.nodeModulesPaths = [
+  path.resolve(projectRoot, 'node_modules'),
+  path.resolve(workspaceRoot, 'node_modules'),
+];
+// 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
+// config.resolver.disableHierarchicalLookup = true;
+
+module.exports = withNativeWind(config, { input: './global.css', inlineRem: 16 });
