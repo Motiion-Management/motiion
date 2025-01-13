549fc31077cb83b5f99f482a19112c0654c87906
panzacoder <jake@plfx.studio>
2025-01-13 11:53:37 -0500

    feat: add landing screen background image and update layout, remove settings modal and route config

--- apps/native/app/_layout.tsx
+++ apps/native/app/_layout.tsx
@@ -4,13 +4,14 @@ import { ActionSheetProvider } from '@expo/react-native-action-sheet';
 import { BottomSheetModalProvider } from '@gorhom/bottom-sheet';
 import { ThemeProvider as NavThemeProvider } from '@react-navigation/native';
 import { Icon } from '@roninoss/icons';
-import { Link, Stack } from 'expo-router';
+import { Link, Stack, ScreenProps } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { Pressable, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { KeyboardProvider } from 'react-native-keyboard-controller';
 
 import { ThemeToggle } from '~/components/ThemeToggle';
+import { Text } from '~/components/nativewindui/Text';
 import ConvexClientProvider from '~/components/providers/ConvexClientProvider';
 import { cn } from '~/lib/cn';
 import { useColorScheme, useInitialAndroidBarSync } from '~/lib/useColorScheme';
@@ -40,9 +41,24 @@ export default function RootLayout() {
             <BottomSheetModalProvider>
               <ActionSheetProvider>
                 <NavThemeProvider value={NAV_THEME[colorScheme]}>
-                  <Stack screenOptions={SCREEN_OPTIONS}>
-                    <Stack.Screen name="index" options={INDEX_OPTIONS} />
-                    <Stack.Screen name="modal" options={MODAL_OPTIONS} />
+                  <Stack
+                    screenOptions={{
+                      animation: 'ios_from_right', // for android
+                    }}>
+                    <Stack.Screen
+                      name="index"
+                      options={{
+                        headerTransparent: true,
+                        title: 'motiion',
+                        headerTitle: ({ children }) => (
+                          <View className="mt-[25%] flex w-full items-center ">
+                            <Text variant="largeTitle" color="primary">
+                              {children}
+                            </Text>
+                          </View>
+                        ),
+                      }}
+                    />
                   </Stack>
                 </NavThemeProvider>
               </ActionSheetProvider>
@@ -55,35 +71,3 @@ export default function RootLayout() {
     </>
   );
 }
-
-const SCREEN_OPTIONS = {
-  animation: 'ios_from_right', // for android
-} as const;
-
-const INDEX_OPTIONS = {
-  headerLargeTitle: true,
-  title: 'NativeWindUI',
-  headerRight: () => <SettingsIcon />,
-} as const;
-
-function SettingsIcon() {
-  const { colors } = useColorScheme();
-  return (
-    <Link href="/modal" asChild>
-      <Pressable className="opacity-80">
-        {({ pressed }) => (
-          <View className={cn(pressed ? 'opacity-50' : 'opacity-90')}>
-            <Icon name="cog-outline" color={colors.foreground} />
-          </View>
-        )}
-      </Pressable>
-    </Link>
-  );
-}
-
-const MODAL_OPTIONS = {
-  presentation: 'modal',
-  animation: 'fade_from_bottom', // for android
-  title: 'Settings',
-  headerRight: () => <ThemeToggle />,
-} as const;
--- apps/native/app/index.tsx
+++ apps/native/app/index.tsx
@@ -1,7 +1,7 @@
 import { SignedIn, SignedOut } from '@clerk/clerk-expo';
 import { Link, Redirect } from 'expo-router';
 import * as React from 'react';
-import { Image, Platform, View } from 'react-native';
+import { Image, ImageBackground, Platform, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { AppleButton } from '~/components/auth/AppleButton';
@@ -23,37 +23,45 @@ export default function RootScreen() {
         <Redirect href="/home" />
       </SignedIn>
       <SignedOut>
-        <SafeAreaView style={{ flex: 1 }}>
-          <View className="ios:justify-end flex-1 justify-center gap-4 px-6 py-4">
-            <View className="items-center">
-              <Image
-                source={LOGO_SOURCE}
-                className="ios:h-12 ios:w-12 h-8 w-8"
-                resizeMode="contain"
-              />
-            </View>
-            <View className="ios:pb-5 ios:pt-2 pb-2">
-              <Text className="ios:font-extrabold text-center text-2xl font-medium">The dance</Text>
-              <Text className="ios:font-extrabold text-center text-2xl font-medium">
-                ecosystem is in
-              </Text>
-              <Text className="ios:font-extrabold text-center text-3xl font-medium">motiion</Text>
-            </View>
-            <Link href="/auth/(create-account)" asChild>
-              <Button size={Platform.select({ ios: 'lg', default: 'md' })}>
-                <Text>Sign up free</Text>
-              </Button>
-            </Link>
+        <ImageBackground
+          source={require('./background.png')}
+          className="h-full w-full"
+          // contentFit="cover"
+        >
+          <SafeAreaView style={{ flex: 1 }}>
+            <View className="ios:justify-end flex-1 justify-center gap-4 px-6 py-4">
+              <View className="items-center">
+                <Image
+                  source={LOGO_SOURCE}
+                  className="ios:h-12 ios:w-12 h-8 w-8"
+                  resizeMode="contain"
+                />
+              </View>
+              <View className="ios:pb-5 ios:pt-2 pb-2">
+                <Text className="ios:font-extrabold text-center text-2xl font-medium">
+                  The dance
+                </Text>
+                <Text className="ios:font-extrabold text-center text-2xl font-medium">
+                  ecosystem is in
+                </Text>
+                <Text className="ios:font-extrabold text-center text-3xl font-medium">motiion</Text>
+              </View>
+              <Link href="/auth/(create-account)" asChild>
+                <Button size={Platform.select({ ios: 'lg', default: 'md' })}>
+                  <Text>Sign up free</Text>
+                </Button>
+              </Link>
 
-            <GoogleButton />
-            <AppleButton />
-            <Link href="/auth/(login)" asChild>
-              <Button variant="plain" size={Platform.select({ ios: 'lg', default: 'md' })}>
-                <Text className="text-primary">Log in</Text>
-              </Button>
-            </Link>
-          </View>
-        </SafeAreaView>
+              <GoogleButton />
+              <AppleButton />
+              <Link href="/auth/(login)" asChild>
+                <Button variant="plain" size={Platform.select({ ios: 'lg', default: 'md' })}>
+                  <Text className="text-primary">Log in</Text>
+                </Button>
+              </Link>
+            </View>
+          </SafeAreaView>
+        </ImageBackground>
         <AlertAnchor ref={alertRef} />
       </SignedOut>
     </>
