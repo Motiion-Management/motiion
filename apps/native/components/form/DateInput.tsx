002b42ccaa35ea54613e180838b14f74ad575900
panzacoder <jake@plfx.studio>
2025-07-06 20:36:37 -0500

    simplify date input

--- apps/native/components/form/DateInput.tsx
+++ apps/native/components/form/DateInput.tsx
@@ -18,15 +18,15 @@ interface DateInputProps {
 
 export const DateInput = ({ label, minimumDate, maximumDate, helpText }: DateInputProps) => {
   const field = useFieldContext<Date>();
-  const [showDatePicker, setShowDatePicker] = useState(false);
+  // const [showDatePicker, setShowDatePicker] = useState(false);
 
-  const formatDate = (date: Date) => {
-    return date.toLocaleDateString('en-US', {
-      month: '2-digit',
-      day: '2-digit',
-      year: 'numeric',
-    });
-  };
+  // const formatDate = (date: Date) => {
+  //   return date.toLocaleDateString('en-US', {
+  //     month: '2-digit',
+  //     day: '2-digit',
+  //     year: 'numeric',
+  //   });
+  // };
 
   return (
     <View className="flex-1">
@@ -37,40 +37,22 @@ export const DateInput = ({ label, minimumDate, maximumDate, helpText }: DateInp
           </Text>
         </View>
       )}
-      <Pressable
-        onPress={() => setShowDatePicker(true)}
-        className={cn(
-          'flex-row items-center justify-between rounded-full bg-surface-high px-4 py-4',
-          field.state.meta.errors?.[0] && 'bg-surface-error'
-        )}>
-        <Text
-          variant="body"
-          className={cn(
-            'text-text-default',
-            field.state.meta.errors?.[0] && 'text-text-error',
-            !field.state.value && 'text-text-default/40'
-          )}>
-          {field.state.value ? formatDate(field.state.value) : 'MM / DD / YYYY'}
-        </Text>
-        <Icon name="chevron-down" size={20} />
-      </Pressable>
-      {showDatePicker && (
-        <DatePicker
-          value={field.state.value || new Date()}
-          mode="date"
-          maximumDate={maximumDate}
-          minimumDate={minimumDate}
-          onChange={(event, selectedDate) => {
-            if (Platform.OS === 'android') {
-              setShowDatePicker(false);
-            }
-            if (selectedDate) {
-              field.handleChange(selectedDate);
-              field.handleBlur();
-            }
-          }}
-        />
-      )}
+
+      <DatePicker
+        value={field.state.value || new Date()}
+        mode="date"
+        maximumDate={maximumDate}
+        minimumDate={minimumDate}
+        onChange={(event, selectedDate) => {
+          // if (Platform.OS === 'android') {
+          //   setShowDatePicker(false);
+          // }
+          if (selectedDate) {
+            field.handleChange(selectedDate);
+            field.handleBlur();
+          }
+        }}
+      />
       {(helpText || field.state.meta.errors?.[0]) && (
         <View className="mt-1 px-4">
           <Text
